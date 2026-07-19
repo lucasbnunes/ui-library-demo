f5553a9efd741b32eb674443040a27ac1554f40f
Lucas Brito <lucasbnunes23@gmail.com>
2026-07-19 15:17:26 -0300

    feat: create input component

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,5 @@ import "./index.css";
 
 export { Button } from "./components/button";
 export type { ButtonProps } from "./components/button";
+export { Input } from "./components/input";
+export type { InputProps } from "./components/input";
